8a193785dcd9ffe6cadcaafbbd68def812233d35
Dimitris Katikaridis <dkatikaridis@gmail.com>
2016-10-21 15:42:43 +0300

    envsetup:remove default devices
    We are opt-cm-13.0
    
    Change-Id: Ieb3d7889e44caef9e4e9f235b2420af3ad2a68a9

--- envsetup.sh
+++ envsetup.sh
@@ -496,12 +496,7 @@ function add_lunch_combo()
 }
 
 # add the default one here
-add_lunch_combo aosp_arm-eng
-add_lunch_combo aosp_arm64-eng
-add_lunch_combo aosp_mips-eng
-add_lunch_combo aosp_mips64-eng
-add_lunch_combo aosp_x86-eng
-add_lunch_combo aosp_x86_64-eng
+#No defaults.we are optcm13.0
 
 function print_lunch_menu()
 {
