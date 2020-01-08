4PERTURE/build

branding: Back to Evolution X!
Change-Id: Iafc7daff5c729476f1fd8bc03cf4c1a3ef75e88e

tools/buildinfo.sh
@@ -61,6 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "org.pixelexperience.device=$CUSTOM_DEVICE"
+echo "org.evolution.device=$CUSTOM_DEVICE"
 
 echo "# end build properties"

unlegacy-devices/android_build-1

tools/buildinfo.sh
@@ -61,6 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "org.pixelexperience.device=$CUSTOM_BUILD"
+echo "org.evolution.device=$CUSTOM_BUILD"
 
 echo "# end build properties"