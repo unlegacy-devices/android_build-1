1690ddca3db0278a5863bc0a7ac870e0a853bcdb
dkati <dkatikaridis@gmail.com>
2020-01-10 22:22:00 +0200

    releasetools: Where is your warranty dude ?

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -2686,8 +2686,8 @@ class BlockDifference(object):
   def WriteScript(self, script, output_zip, progress=None,
                   write_verify_script=False):
     if not self.src:
-      # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
+      script.Print(" ")
+      script.Print("Where is your warranty dude ?")
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
