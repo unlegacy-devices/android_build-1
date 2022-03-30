942c059630c0138fb4cfee07294daddb9a655ee2
dkati <dkatikaridis@gmail.com>
2022-03-31 00:42:17 +0300

    sysprop.mk: Show our ROM name under settings/about phone

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -215,6 +215,8 @@ else
   BUILD_DISPLAY_ID := $(BUILD_DESC)
 endif
 
+BUILD_DISPLAY_ID := Optimized-LineageOS-19.1-Version-$(OPTIMIZED_LINEAGEOS_VERSION)
+  
 # TARGET_BUILD_FLAVOR and ro.build.flavor are used only by the test
 # harness to distinguish builds. Only add _asan for a sanitized build
 # if it isn't already a part of the flavor (via a dedicated lunch
